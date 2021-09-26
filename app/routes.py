5702848d6351ebeca62b633bc6a24b95ac4b0832
Luan Esdras Rangel <luanesdras.rangel@gmail.com>
2021-09-26 15:18:00 -0300

    Implementado os Models de usuários e posts.

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from flask import render_template
+from flask.helpers import flash
+from werkzeug.utils import redirect
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'kud'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested form user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('index')
+    return render_template('login.html', title='Sing in', form=form)
