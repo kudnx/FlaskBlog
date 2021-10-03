f24aeb072ed2dce19e2881e8c16e9d3e6a569285
Luan Esdras Rangel <luanesdras.rangel@gmail.com>
2021-10-02 21:05:52 -0300

    Implementado busca de posts.

--- app/main/forms.py
+++ app/main/forms.py
@@ -25,3 +25,13 @@ class EmptyForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField('Escreva algo:', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+class SearchForm(FlaskForm):
+    q = StringField(('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
